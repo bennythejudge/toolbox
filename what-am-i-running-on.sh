4e5c299c8f948b665b101b993c634c12d1692934
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-10 02:00:01 +0200

    Commit work

--- what-am-i-running-on.sh
+++ what-am-i-running-on.sh
@@ -14,12 +14,13 @@ echo "uname -a: $(uname -a)"
 
 if [[ -e /etc/redhat-release ]]; then
         echo "Smells like a RH distro - content of /etc/redhat-release"
-        echo "content of /etc/redhat-release: $(cat /etc/redhat-release)"
+        cat /etc/redhat-release
 elif [[ -e /etc/debian_version ]]; then
-        echo "It seems we are in debian land"
-        echo "content of /etc/debian_version: $(cat /etc/debian_version)"
-else
-        echo "Neither RH nor Debian - what am I?"
+        echo "It seems we are in debian land - content of /etc/debian_version"
+        cat /etc/debian_version
+elif [[ -e /etc/os-release ]]; then
+	echo "I found /etc/os-release"
+	cat /etc/os-release
 fi
 
 echo "$(date) - $(basename $0) - completed"
