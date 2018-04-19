e7e171749656111f960f283179af36198209db91
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-20 00:16:56 +0200

    Add checks to the Linux detector script

--- what-am-i-running-on.sh
+++ what-am-i-running-on.sh
@@ -13,10 +13,10 @@ fi
 echo "uname -a: $(uname -a)"
 
 if [[ -e /etc/redhat-release ]]; then
-        echo "Smells like a RH distro - content of /etc/redhat-release"
+        echo "RH distro - content of /etc/redhat-release"
         cat /etc/redhat-release
 elif [[ -e /etc/debian_version ]]; then
-        echo "It seems we are in debian land - content of /etc/debian_version"
+        echo "Debian - content of /etc/debian_version"
         cat /etc/debian_version
 elif [[ -e /etc/os-release ]]; then
 	echo "I found /etc/os-release"
