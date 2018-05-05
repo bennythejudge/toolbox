78806af4201e6e946f3e7dd0f5e04018a0840115
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-05-05 08:37:09 +0200

    Add AWS shell extensions

---
+++ aws_shell_extensions.sh
@@ -0,0 +1,7 @@
+# source this file
+
+# list all ec2 - will use whatever authentication/authorization you have in the shell
+# for example ~/.aws/credentials the default set of credentials including region
+awsec2ls () {
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,State.Name,InstanceType,PrivateIpAddress,PublicIpAddress,Tags[?Key==`Name`].Value[]]' --output json | tr -d '\n[] "' | perl -pe 's/i-/\ni-/g' | tr ',' '\t' | sed -e 's/null/None/g' | grep '^i-' | column -t
+}
